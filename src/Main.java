9cbf9c6ca031b40792833158bacb2edaeb7f942e
Bri Macías <brina.padilha@gmail.com>
2022-11-21 16:31:36 +0100

    añadiendo cñodigo para probar las propiedades de Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Empleado p = new Empleado("Pepe Pérez", 30);
+        System.out.println(p);
+
+        p.nombre = "Otro nombre";
+        System.out.println(p);
     }
 }
